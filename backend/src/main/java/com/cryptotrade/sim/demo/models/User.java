aa2f92c3f1fd023de4ec3d9c78ae85b703b5d1c0
Ivaylo Gavrailov <igavrailov142@gmail.com>
2025-03-26 18:35:39 +0200

    first commit

---
+++ backend/src/main/java/com/cryptotrade/sim/demo/models/User.java
@@ -0,0 +1,42 @@
+package com.cryptotrade.sim.demo.models;
+
+import java.math.BigDecimal;
+
+public class User {
+    private int id;
+    private String name;
+    private BigDecimal balance;
+
+    public User(int id, String name, BigDecimal balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    
+    
+}
---
+++ backend/src/main/java/com/cryptotrade/sim/demo/repos/UsersRepository.java
@@ -0,0 +1,34 @@
+package com.cryptotrade.sim.demo.repos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.cryptotrade.sim.demo.models.User;
+
+@Repository
+public class UsersRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UsersRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<User> getAllUsersList() {
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, new UserRowMapper());
+    }
+}
+
+
+class UserRowMapper implements RowMapper<User> {
+    
+    public User mapRow(ResultSet rs, int row) throws SQLException {
+        return new User(rs.getInt("id"), rs.getString("name"), rs.getBigDecimal("balance"));
+    }
+}
+
